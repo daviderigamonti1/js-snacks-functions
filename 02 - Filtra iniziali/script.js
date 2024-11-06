5d99792a8ed983bb1ff069d2f98533844e066d38
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-11-06 15:22:15 +0100

    declare function on "filtra iniziali"

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,9 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function printInitials(names, letter) {
+    //Array in cui inserirò i nomi filtrati
+    let initialNames = [];
+    //Ciclo su tutti i nomi
+    for(let i = 0; i < names.length; i++) {
+        //Condizione per confrontare la prima lettera dei nomi con la lettera data dall'utente
+        if (names[i][0].toUpperCase() === letter.toUpperCase()) {
+            initialNames.push(names[i]);
+        }
+    }
+    return initialNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
 
 
 
