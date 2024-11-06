dfaf714d8154994527c1cb3513309fb71253bb74
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-11-06 14:19:22 +0100

    declare function on "iniziali"

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function printName(names) {
+    //Inizializzo un array vuoto in cui andrò poi ad inserire le iniziali dei nomi
+    let initialNames = [];
+    //Ciclo for per prendere tutti i nomi uno ad uno
+    for (let i = 0; i < names.length; i++) {
+        initialNames.push(names[i][0].toUpperCase());
+    }
+    return initialNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
