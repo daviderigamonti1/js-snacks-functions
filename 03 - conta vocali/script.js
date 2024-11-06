a4e699f33110207ca4217da7b0586cfd44e7100d
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-11-06 15:52:54 +0100

    declare function on "conta vocali"

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,10 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
+//Inizializzo le variabili
 const word = 'javascript';
-
+const vowels = "aeiou";
 
 // Dichiara la funzione qui.
-
+function printVowels(word) {
+    //Inizializzo la variabile per contare il numero di vocali
+    let vowelCount = 0;
+    //Ciclo per tutte le lettere di word
+    for (let i = 0; i < word.length; i++) {
+        //Condizione per contare le vocali
+        if (vowels.includes(word[i])) {
+            vowelCount ++;
+        }
+    }
+    return vowelCount;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
