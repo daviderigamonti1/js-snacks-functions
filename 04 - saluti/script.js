ebe96a030f858afc94794d337ee951f30cc01009
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-11-06 16:04:48 +0100

    saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,14 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function hello(name) {
+    let salutation = (`Hello ${name}`);
+    return salutation;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = hello(name);
+console.log(result);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
